5a8fe392e404b4e9d0ce8910aa81aa71dc83f021
Luis Guzman <lumigu.dev@gmail.com>
2025-10-14 23:34:05 -0500

    ♻️ Refactor LLM client creation

--- app/services/agent.py
+++ app/services/agent.py
@@ -1,20 +1,11 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
-from app.core.config import settings
+from ..core.config import settings
 
 
-chat_model = ChatOpenAI(
-    model="gpt-4o-mini",
-    temperature=0.7,
-    max_tokens=100,
-    max_completion_tokens=100,
-    api_key=settings.OPENAI_API_KEY,
-)
-
-
-def generate_response(query: str):
-    print(chat_model.invoke([HumanMessage(content=query)]))
-    # return chat_model.invoke([HumanMessage(content=query)])
-
-
-generate_response("¿Qué es RAG en una sentencia corta?")
+def create_llm(temperature: float = 0.7) -> ChatOpenAI:
+    """Crea cliente LLM"""
+    return ChatOpenAI(
+        api_key=settings.OPENAI_API_KEY,
+        model="gpt-4o-mini",
+        temperature=temperature,
+    )
